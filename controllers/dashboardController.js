ca69760e5ced0196e1dd953332b81a76cf87dfb9
reza <rezarezaeijamiuk@gmail.com>
2022-06-13 14:11:37 +0100

    working on edit user method validation

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -14,6 +14,12 @@ module.exports = new class dashboardController extends controller {
 
   async edituser(req, res, next) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        let myErrors = errors.array().map((err) => err.msg);
+        req.flash("errors", myErrors);
+        return res.redirect("/dashboard");
+      }
       console.log(req.body);
     } catch (err) {
       next(err);
