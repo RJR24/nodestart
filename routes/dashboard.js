85219203d484031e4639462c709db4a64af7b15e
reza <rezarezaeijamiuk@gmail.com>
2022-06-13 14:09:30 +0100

    working on edit user router (adding codes for profile pic upload)

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,16 +4,29 @@ const router = express.Router();
 ///controllers
 const dashboardController = require("controllers/dashboardController");
 const editUserValidatoer = require("validators/editUserValidatoer");
+const uploadUserProfile = require("upload/uploadUserProfile");
 
-router.use((req,res,next)=>{
-  if(req.isAuthenticated()){
+router.use((req, res, next) => {
+  if (req.isAuthenticated()) {
     return next();
-  } 
-  res.redirect('/');
-})
+  }
+  res.redirect("/");
+});
 
 router.get("/", dashboardController.index);
-router.post("/edituser", dashboardController.edituser);
-
+router.post(
+  "/edituser",
+  uploadUserProfile.single("img"),
+  (req, res, next) => {
+    if (!req.file) {
+      req.body.img = null;
+    } else {
+      req.body.img = req.file.filename;
+    }
+    next();
+  },
+  editUserValidatoer.handle(),
+  dashboardController.edituser
+);
 
 module.exports = router;
