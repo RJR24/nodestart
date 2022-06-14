893be278784c1235b313399b45620d8011ce60f5
reza <rezarezaeijamiuk@gmail.com>
2022-06-14 13:02:07 +0100

    add img extention validation

--- validators/editUserValidatoer.js
+++ validators/editUserValidatoer.js
@@ -1,11 +1,20 @@
 const validator = require("./validator");
 const { check } = require("express-validator");
+const path = require("path");
 
 module.exports = new (class editUserValidator extends validator {
   handle() {
     return [
       check("name", "name field can not be empty!").not().isEmpty(),
       check("img", "profile picture is required!").not().isEmpty(),
+      check("img").custom(async (value) => {
+        if (!value) {
+          return;
+        }
+        if (![".jpg", ".jpeg", ".png"].includes(path.extname(value))) {
+          throw new Error("profile picture extention is not correct!");
+        }
+      }),
     ];
   }
 })();
